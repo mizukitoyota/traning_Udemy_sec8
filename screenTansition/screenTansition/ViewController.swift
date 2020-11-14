6aff4942325ace16f97db9b55070e8ca3b7c230f
MizukiToyota <mtoyota@system-renovate.co.jp>
2020-11-14 16:22:58 +0900

    ファイル作成

---
+++ screenTansition/screenTansition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  screenTansition
+//
+//  Created by Training on 2020/11/14.
+//  Copyright © 2020 training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
